84b3eac7aef8dfb64593e21bad9732c25127c2d1
Andrew Marfo <andrew.marfo@amalitech.com>
2024-12-02 22:19:46 +0000

    Airflow dag working

--- dags/weather_pipeline.py
+++ dags/weather_pipeline.py
@@ -1,13 +1,13 @@
 from airflow import DAG
 from airflow.operators.http_operator import SimpleHttpOperator
 from airflow.sensors.http_sensor import HttpSensor
+from airflow.hooks.base import BaseHook
 from airflow.operators.python_operator import PythonOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 import requests
 import json
-from pytz import timezone
 from dotenv import load_dotenv
 import os
 
@@ -29,20 +29,22 @@ default_args = {
     'retries': 1,
     'retry_delay': timedelta(minutes=2),
 }
+weather_connection = BaseHook.get_connection(api_conn_id)
+api_key = weather_connection.extra_dejson.get('api_key')
 
 with DAG(
     'weather_data_pipeline',
     default_args=default_args,
     description='A dag to fetch and store daily weather data for Portland',
     schedule_interval='@daily',
-    start_date=datetime(2024, 12, 2),
+    start_date=datetime(2024, 11, 28),
     catchup=False,
 ) as dag:
     
     check_weather_api = HttpSensor(
         task_id='check_weather_api',
         http_conn_id=api_conn_id,
-        endpoint='data/2.5/weather?q=Portland&appid={{ var.value.api_key }}',
+        endpoint=f'data/2.5/weather?q=Portland&APPID={api_key}',
         response_check=lambda response: response.status_code == 200,
         poke_interval=5,
         timeout=20,
@@ -54,15 +56,8 @@ with DAG(
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
-            portland_weather_data = {
-                'id': data['id'],
-                'city': data['name'],   
-                'temperature': data['main']['temp'],
-                'pressure': data['main']['pressure'],
-                'humidity': data['main']['humidity'],
-            }
-            ti.xcom_push(key="p_weather_data", value=portland_weather_data)
-            return portland_weather_data
+            ti.xcom_push(key="p_weather_data", value=data)
+            return data
         else:
             raise Exception(f'Failed to fetch weather. Status code: {response.status_code}')
     
@@ -72,16 +67,27 @@ with DAG(
         dag=dag,
     )
 
-    # Transforming the data to a more suitable format (Temperature)
+    # Transforming the data to a more suitable format and converting temperature from Kelvin to Fahrenheight
     def transform_data(ti):
         weather_data = ti.xcom_pull(key="p_weather_data", task_ids='fetch_weather_data')
+        city = weather_data['name']
+        temperature_k = weather_data['main']['temp']
 
         # Transforming temperature to Fahrenheit
-        weather_data['temperature'] = (weather_data['temperature'] - 273.15) * 9/5 + 32
+        temperature_f = (temperature_k - 273.15) * 9/5 + 32
+        pressure = weather_data['main']['pressure']
+        humidity = weather_data['main']['humidity']
+        timestamp = datetime.fromtimestamp(weather_data['dt'], timezone.utc)
+        portland_weather_data = {
+            'city': city,   
+            'temperature': temperature_f,
+            'pressure': pressure,
+            'humidity': humidity,
+            'timestamp': timestamp,
+        }
 
-
-        ti.xcom_push(key="p_weather_data", value=weather_data)
-        return weather_data
+        ti.xcom_push(key="p_weather_data", value=portland_weather_data)
+        return portland_weather_data
 
     transform_weather_data = PythonOperator(
         task_id='transform_weather_data',
@@ -89,55 +95,35 @@ with DAG(
         dag=dag,
     )
 
-    create_table = PostgresOperator(
-        task_id='create_table',
-        postgres_conn_id=postgres_database_conn_id,
-        sql="""
-        CREATE TABLE IF NOT EXISTS weather_data.daily_weather (
-            id INTEGER PRIMARY KEY,
-            city TEXT NOT NULL,
-            temperature DOUBLE PRECISION NOT NULL,
-            pressure INTEGER NOT NULL,
-            humidity INTEGER NOT NULL,
-            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        );
-        """,
-        dag=dag,
-    )
-
     # Storing weather data in postgres database
     def load_to_postgres(ti):
-        weather_data = ti.xcom_pull(key="p_weather_data", task_ids='transform_weather_data')
-        if not weather_data:
-            raise ValueError("Weather data not found")
-            
+        weather_data = ti.xcom_pull(key="p_weather_data", task_ids='transform_weather_data')     
         postgres_hook = PostgresHook(postgres_conn_id=postgres_database_conn_id)
         conn = postgres_hook.get_conn()
         cursor = conn.cursor()
 
         #Create table if it does not exist
         cursor.execute("""
-        CREATE TABLE IF NOT EXISTS daily_weather (
-            id INTEGER PRIMARY KEY,
-            city TEXT NOT NULL,
-            temperature DOUBLE PRECISION NOT NULL,
-            pressure INTEGER NOT NULL,
-            humidity INTEGER NOT NULL,
-            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        );
+            CREATE TABLE IF NOT EXISTS daily_weather (
+                city TEXT NOT NULL,
+                temperature DOUBLE PRECISION NOT NULL,
+                pressure INTEGER NOT NULL,
+                humidity INTEGER NOT NULL,
+                timestamp TIMESTAMP NOT NULL
+            );
         """)
 
         #Inserting transformed data into table
         insert_query = """
-        INSERT INTO  weather_data.daily_weather (id, city, temperature, pressure, humidity)
+        INSERT INTO  daily_weather (city, temperature, pressure, humidity, timestamp)
         VALUES (%s, %s, %s, %s, %s);
         """
-        cursor.execute(insert_query,(
-            weather_data['id'], 
+        cursor.execute(insert_query,( 
             weather_data['city'], 
             weather_data['temperature'], 
             weather_data['pressure'], 
-            weather_data['humidity']
+            weather_data['humidity'],
+            weather_data['timestamp'],
         ))
 
         conn.commit()
@@ -150,4 +136,5 @@ with DAG(
     )
 
 # Dependencies
-check_weather_api >> fetch_weather_data >> transform_weather_data >>  create_table >> load_weather_data
+# check_weather_api >> 
+fetch_weather_data >> transform_weather_data  >> load_weather_data
